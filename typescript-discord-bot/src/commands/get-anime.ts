723226bd4f9c78210eaa5d28dc10105415f0cf55
Francisco Tavares <francisco.tavares@asml.com>
2023-03-29 15:12:48 +0200

    Trying to connect command with my-anime-list servce

--- typescript-discord-bot/src/commands/get-anime.ts
+++ typescript-discord-bot/src/commands/get-anime.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, Client, ApplicationCommandType, ApplicationCommandOptionType } from "discord.js";
+import { CommandInteraction, Client, ApplicationCommandType, ApplicationCommandOptionType, MessagePayload } from "discord.js";
 import { getAnimeList } from "src/external/my-anime-list";
 import { Command } from "../command";
 
@@ -21,13 +21,12 @@ export const GetAnime: Command = {
         }
     ],
     run: async (_client: Client, interaction: CommandInteraction) => {
-        console.log(interaction.options);
         console.log(interaction.options.data);
-        const animeName = interaction.options.get('animeName');
-        const nrOfResults = interaction.options.get('nrOfResults');
+        const animeName = interaction.options.data[0].value as string;
+        const nrOfResults = interaction.options.data[1]?.value as number || undefined;
 
         // Here I still need to do the actual call to my-anime-list service
-        // const animes = getAnimeList(animeName, nrOfResults);
+        const anime = getAnimeList(animeName, nrOfResults);
 
         const content = `You are currently earching for ${nrOfResults} items matching your query: ${animeName}!`;
         await interaction.followUp({
