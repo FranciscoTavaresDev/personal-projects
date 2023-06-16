57b6b677d0fa0f5917f463f54756987b707afd73
Francisco Tavares <francisco.tavares@asml.com>
2023-06-16 13:13:03 +0200

    Added docker-compose file changes to instantiate a mysql image

--- typescript-inventory-backend/inventory-service/src/services/computer-processing-unit-service/computer-processing-unit-router.ts
+++ typescript-inventory-backend/inventory-service/src/services/computer-processing-unit-service/computer-processing-unit-router.ts
@@ -1,5 +1,7 @@
 
 import express, { type Request, type Response } from 'express'
+import { myDataSource } from '../../app-data-source'
+import { ComputerProcessingUnit } from './models/computer-processing-unit'
 
 export const computerProcessingUnitRoute = express.Router()
 
@@ -11,7 +13,9 @@ computerProcessingUnitRoute.post('/cpu', (req: Request, res: Response): void => 
   })
 })
 
-computerProcessingUnitRoute.get('/cpu', (req: Request, res: Response): void => {
+computerProcessingUnitRoute.get('/cpu', async (req: Request, res: Response): Promise<void> => {
+  const cPUs = await myDataSource.getRepository(ComputerProcessingUnit).find()
+  res.json(cPUs)
   res.json({
     status: 200,
     success: true,
